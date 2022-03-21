5285e6d9a2ada22af1e5e64f5e28c2910bca4379
Christoph Reichenbach <creichen@gmail.com>
2022-03-21 19:52:53 +0000

    Test merge fixes

--- test/papi/EventSetApiTest.java
+++ test/papi/EventSetApiTest.java
@@ -65,29 +65,6 @@ public class EventSetApiTest {
                 ev.stop();
         }
 
-        @BeforeClass
-        public static void initialization() {
-                Papi.init();
-        }
-
-        @Test
-        public void
-        nullArgumentForCreatingEventSetFails() {
-                assertEquals(Constants.PAPI_EINVAL, Wrapper.eventSetCreate(null));
-        }
-
-        @Test
-        public void
-        emptyArrayArgumentForCreatingEventSetFails() {
-                assertEquals(Constants.PAPI_EINVAL, Wrapper.eventSetCreate(new long[0]));
-        }
-
-        @Test
-        public void
-        tooBigArrayArgumentForCreatingEventSetFails() {
-                assertEquals(Constants.PAPI_EINVAL, Wrapper.eventSetCreate(new long[2]));
-        }
-
         public static BigInteger
         fib(int n) {
                 if (n == 0) {
